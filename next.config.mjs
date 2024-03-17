e95227d5bb6bae5e8b7063818833a8343d4f9ee2
sajib37 <sajibhasan9811@gmail.com>
2024-03-18 00:18:05 +0600

    git problem

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
     images: {
         domains: ['i.ibb.co']
     }
-=======
-  images: {
-    domains: ["i.ibb.co"],
-  },
->>>>>>> 3d8df46e682f54c1e6bbd8260a943a339f175083
 };
 
 export default nextConfig;
