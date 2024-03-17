89355b3f2a834d7a3d38e5157e28d73fd228f75e
sajib37 <sajibhasan9811@gmail.com>
2024-03-17 23:43:40 +0600

    check

--- src/app/guider/page.jsx
+++ src/app/guider/page.jsx
@@ -3,7 +3,6 @@ import data from "../utils/data.json"
 import SpecialistCard from "../Components/SpecialistCard";
 const Guider = () => {
   return (
-<<<<<<< HEAD
     <section className="max-w-screen-xl mx-auto">
       
       <section className="grid md:grid-cols-3 lg:grid-cols-4 gap-4">
@@ -12,12 +11,6 @@ const Guider = () => {
         }
       </section>
     </section>
-=======
-    <div>
-      <p>Guider</p>
-    </div>
-    
->>>>>>> 611c8e59b87024c4f60788e686cb96b79fdc8a5c
   )
 };
 
